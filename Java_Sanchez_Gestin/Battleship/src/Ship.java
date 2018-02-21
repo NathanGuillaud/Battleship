3c8ec00123064d22271b91161c346ebb3586cc2c
Nathan Guillaud <nathan.guillaud@orange.fr>
2018-02-21 17:18:56 +0100

    Debut classe Ship

---
+++ Java_Sanchez_Gestin/Battleship/src/Ship.java
@@ -0,0 +1,34 @@
+
+public class Ship {
+	private String startCoord;
+	private String endCoord;
+
+	public String getStartCoord() {
+		return startCoord;
+	}
+
+	public void setStartCoord(String startCoord) {
+		this.startCoord = startCoord;
+	}
+
+	public String getEndCoord() {
+		return endCoord;
+	}
+
+	public void setEndCoord(String endCoord) {
+		this.endCoord = endCoord;
+	}
+
+	public Ship(String startCoord, String endCoord) {
+		this.startCoord = startCoord;
+		this.endCoord = endCoord;
+	}
+	
+	public String[] getShipPos(){
+		if this.startCoord.substring(0, 1)
+	}
+
+	public bool isHit(String missileCoord){
+		if
+	}
+}
